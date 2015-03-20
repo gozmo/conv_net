a394a19dcf69d940a26b8f06207b4ccb41ac23b7
goz <gozmo1337@gmail.com>
2015-03-20 21:25:53 +0100

    moved flipbatchiterator to basicnetwork

--- networks/basic_network.py
+++ networks/basic_network.py
@@ -1,9 +1,11 @@
 import cPickle as pickle
+import numpy as np
 from lasagne import layers
 from lasagne.updates import nesterov_momentum
 from nolearn.lasagne import NeuralNet
 from sklearn.metrics import mean_squared_error
 import sys
+from nolearn.lasagne import BatchIterator
 
 #to be able to store big networks
 sys.setrecursionlimit(10000)
@@ -57,3 +59,31 @@ class BasicNetwork:
     def _save_net(self):
         with open('%s.pickle'% self.name, 'wb') as f:
             pickle.dump(self._net, f, -1)
+
+
+class FlipBatchIterator(BatchIterator):
+    flip_indices = [
+        (0, 2), (1, 3),
+        (4, 8), (5, 9), (6, 10), (7, 11),
+        (12, 16), (13, 17), (14, 18), (15, 19),
+        (22, 24), (23, 25),
+        ]
+
+    def transform(self, Xb, yb):
+        Xb, yb = super(FlipBatchIterator, self).transform(Xb, yb)
+
+        # Flip half of the images in this batch at random:
+        bs = Xb.shape[0]
+        indices = np.random.choice(bs, bs / 2, replace=False)
+        Xb[indices] = Xb[indices, :, :, ::-1]
+
+        if yb is not None:
+            # Horizontal flip of all x coordinates:
+            yb[indices, ::2] = yb[indices, ::2] * -1
+
+            # Swap places, e.g. left_eye_center_x -> right_eye_center_x
+            for a, b in self.flip_indices:
+                yb[indices, a], yb[indices, b] = (
+                    yb[indices, b], yb[indices, a])
+
+        return Xb, yb
